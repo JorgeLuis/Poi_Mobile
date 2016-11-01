9139f29729ac323cc1dda13f3a8d53d324073e40
CharlyEnriquez <killboymota@gmail.com>
2016-11-01 20:34:43 -0300

    Commit de elementos

---
+++ app/src/main/java/poi/grupo10/usam/poi_mobile/Dominio/Colectivo.java
@@ -0,0 +1,8 @@
+package poi.grupo10.usam.poi_mobile.Dominio;
+
+/**
+ * Created by m0tz on 11/1/16.
+ */
+
+public class Colectivo extends Poi {
+}
--- app/src/main/java/poi/grupo10/usam/poi_mobile/Dominio/Poi.java
+++ app/src/main/java/poi/grupo10/usam/poi_mobile/Dominio/Poi.java
@@ -6,7 +6,44 @@ package poi.grupo10.usam.poi_mobile.Dominio;
 
 public class Poi {
 
-    private int numero;
+    private String Nombre;
+    private String Barrio;
+    private String Direccion;
+    private int Numero;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getBarrio() {
+        return Barrio;
+    }
+
+    public void setBarrio(String barrio) {
+        Barrio = barrio;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
+
 
 
 }
